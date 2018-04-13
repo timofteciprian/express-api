733ac4b1eae34c8e13015d98df64ea156b09fed8
Unknown <timofteca@gmail.com>
2018-04-13 21:08:21 +0300

    added  authorization for books

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,10 +2,17 @@
 const express = require('express');
 const { MongoClient, ObjectID } = require('mongodb');
 const bookRouter = express.Router();
-const sql = require('mssql');
+//const sql = require('mssql');
 // const debug = require('debug')('app:bookRoutes');
 
 function router(nav) {
+  bookRouter.use((req, res, next) => {
+    if (req.user) {
+      next();
+    } else {
+      res.redirect('/');
+    }
+  });
   bookRouter.route('/')
     .get((req, res) => {
       const url = 'mongodb://localhost:27017';
